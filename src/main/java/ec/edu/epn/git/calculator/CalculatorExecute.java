3d03beab93be6b05d834a15155144394f3b0a270
hidalgobri <brigitte.hidalgo@epn.edu.ec>
2021-01-16 19:28:55 -0500

    Clase execute añadida

---
+++ src/main/java/ec/edu/epn/git/calculator/CalculatorExecute.java
@@ -0,0 +1,13 @@
+package ec.edu.epn.git.calculator;
+
+public class CalculatorExecute {
+    public static void main(String[] args)
+    {
+        System.out.println("***Calculator Execute***");
+
+        Calculator c = new Calculator();
+        System.out.println("c.adition(4,7)="+c.addition(4,7));
+        System.out.println("c.substraction(7,1)="+c.subtraction(7,1));
+
+    }
+}
